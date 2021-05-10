9c8a2fb4f6894739aea7be89d019cf9c319ac301
bjdumm <bjdumm23@gmail.com>
2021-05-10 13:09:28 +0000

    Added header file for linked list.

--- C++/Data Structures/Linear/Linked List/ll.cpp
+++ C++/Data Structures/Linear/Linked List/ll.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
-struct Node {
-    Node* n;
+struct  Node {
     int data = 0;
+    Node* n;
 };
 
 
 class LinkedList{
     public:
         void addNode(int data) {
-
+	    Node* node = new Node(data);
             std::cout << "You added " << data << " to the list.";
         }
+
+
 
     private:
         Node* head;
---
+++ C++/Data Structures/Linear/Linked List/ll.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class LinkedList{
+	public:
+		LinkedList();
+		LinkedList(Node* head);
+		LinkedList(int data);
+		void add(int data);
+		int  remove();
+		int insert(int data);
+		int size();
+		bool isEmpty();
+
+
+
+};
+
+
+
