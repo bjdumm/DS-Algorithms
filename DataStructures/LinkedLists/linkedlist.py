cbcecb939c6aa739cb1014286a14a9e525108df9
bjdumm <54458241+bjdumm@users.noreply.github.com>
2021-05-21 15:15:04 -0400

    Update linkedlist.py

--- DataStructures/LinkedLists/linkedlist.py
+++ DataStructures/LinkedLists/linkedlist.py
@@ -1,35 +1,51 @@
 """Class to implement a linked list and test the functions"""
-
 class Node:
-  def __init__(self,data):
-    self.data = data
-    self.next = None
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
     
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.listSize = 0
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
     
-  def addNode(self,node):
-    if (self.head is None):
-      self.head = node
-      self.listSize = 1
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
     else:
-      n = self.head
-      while(n.next != None):
-        n = n.next
-      n.next = node
-      listSize += 1
-  
-  def printList(self):
-    node = self.head
-    while(node != None):
-      print(node.data, sep=" ")
-      node = node.next
-      
-  def getSize(self):
-    return self.listSize
-  
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
   
       
 n1 = Node(1)
@@ -39,4 +55,9 @@ n4 = Node(4)
 
 ll = LinkedList()
 ll.addNode(n1); ll.addNode(n2); ll.addNode(n3); ll.addNode(n4)
+
+
+
+
+
 
