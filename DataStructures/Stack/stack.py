abfa3cc84c8cd04aa0d8563030950c5955dfd13d
bjdumm <54458241+bjdumm@users.noreply.github.com>
2021-05-21 15:22:09 -0400

    Update stack.py

--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -1,23 +1,59 @@
-"""Implement a stack and test its functions"""
+from node import Node
 
-class Node:
-  def __init__(self,data):
-    self.data = data
-    self.next = None
-    
-    
 class Stack:
-  def __init__(self):
-    self.nodes = []
+  def __init__(self, limit=1000):
+    self.top_item = None
     self.size = 0
-    
-  def peek(self):
-    print(self.nodes[self.size - 1])
-    
+    self.limit = limit
   
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+      print("Adding {} to the pizza stack!".format(value))
+    else:
+      print("No room for {}!".format(value))
+
   def pop(self):
-     node = self.nodes[self.size - 1]
-    return 
+    if not self.is_empty():
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      print("Delivering " + item_to_remove.get_value())
+      return item_to_remove.get_value()
+    print("All out of pizza.")
+
+  def peek(self):
+    if not self.is_empty():
+      return self.top_item.get_value()
+    print("Nothing to see here!")
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self):
+    return self.size == 0
   
-  def push(self):
-    pass
+# Defining an empty pizza stack
+stack = Stack(6)
+# Adding pizzas as they are ready until we have 
+stack.push("pizza #1")
+stack.push("pizza #2")
+stack.push("pizza #3")
+stack.push("pizza #4")
+stack.push("pizza #5")
+stack.push("pizza #6")
+
+# Uncomment the push() statement below:
+stack.push("pizza #7")
+
+# Delivering pizzas from the top of the stack down
+print("The first pizza to deliver is " + stack.peek())
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
