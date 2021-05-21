be45048cb30f02b10c9677579427d24fa92e57ea
bjdumm <54458241+bjdumm@users.noreply.github.com>
2021-05-20 22:10:21 -0400

    Create binarytree.py

---
+++ DataStructures/Trees/binarytree.py
@@ -0,0 +1,16 @@
+class Node:
+  def __init__(self):
+    self.data = data
+    self.left = None
+    self.right = None
+    
+class BinaryTree:
+  def __init__(self):
+    self.root = None
+    
+  def add(self,data):
+    n = Node(data)
+    if (root is None):
+      root = n
+    else:
+      
