1a276cac25551532ed8073ba0e2c02c06ade4b89
bjdumm <54458241+bjdumm@users.noreply.github.com>
2021-05-20 21:59:02 -0400

    Create linkedlist.py

---
+++ DataStructures/LinkedLists/linkedlist.py
@@ -0,0 +1,36 @@
+"""Class to implement a linked list and test the functions"""
+
+class Node:
+  def __init__(self,data):
+    self.data = data
+    self.next = None
+    
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.listSize = 0
+    
+  def addNode(self,node):
+    if (self.head is None):
+      self.head = node
+      self.listSize = 1
+    else:
+      n = self.head
+      while(n.next != None):
+        n = n.next
+      n.next = node
+      listSize += 1
+      
+  def getSize(self):
+    return self.listSize
+  
+  
+      
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+
+ll = LinkedList()
+ll.addNode(n1); ll.addNode(n2); ll.addNode(n3); ll.addNode(n4)
+
