9e2b99e7cd534ce85582e4acc8c0a57a129e5aeb
bjdumm <bjdumm45@gmail.com>
2021-12-14 22:47:03 -0500

    Cleaned up stack.py

--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -12,7 +12,7 @@ class Stack:
       item.set_next_node(self.top_item)
       self.top_item = item
       self.size += 1
-      print("Adding {} to the pizza stack!".format(value))
+      print("Adding {} to the stack!".format(value))
     else:
       print("No room for {}!".format(value))
 
@@ -21,14 +21,14 @@ class Stack:
       item_to_remove = self.top_item
       self.top_item = item_to_remove.get_next_node()
       self.size -= 1
-      print("Delivering " + item_to_remove.get_value())
+      print("Popping " + item_to_remove.get_value())
       return item_to_remove.get_value()
-    print("All out of pizza.")
+    print("Stack is empty.")
 
   def peek(self):
     if not self.is_empty():
       return self.top_item.get_value()
-    print("Nothing to see here!")
+    print("Empty stack")
 
   def has_space(self):
     return self.limit > self.size
@@ -36,21 +36,21 @@ class Stack:
   def is_empty(self):
     return self.size == 0
   
-# Defining an empty pizza stack
+# Defining an empty stack
 stack = Stack(6)
-# Adding pizzas as they are ready until we have 
-stack.push("pizza #1")
-stack.push("pizza #2")
-stack.push("pizza #3")
-stack.push("pizza #4")
-stack.push("pizza #5")
-stack.push("pizza #6")
+# Adding items to stack 
+stack.push("First")
+stack.push("Second")
+stack.push("Third")
+stack.push("Fourth")
+stack.push("Fifth")
+stack.push("Sixth")
 
-# Uncomment the push() statement below:
-stack.push("pizza #7")
 
-# Delivering pizzas from the top of the stack down
-print("The first pizza to deliver is " + stack.peek())
+stack.push("Seventh")
+
+# Removing items from the top of the stack down
+print("The top of the stack is " + stack.peek())
 stack.pop()
 stack.pop()
 stack.pop()
